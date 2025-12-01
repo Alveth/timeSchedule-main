2bedbac650c24a9a6d796c6bbb14eb75986f7aa6
Alveth <134678129+Alveth@users.noreply.github.com>
2025-12-01 06:01:55 +0000

    20251201

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import datetime as dt
+import pandas as pd
 
 st.title("Hello your schedule")
 month = dt.date.today().month
@@ -9,3 +10,14 @@ if month <= 4 and month >= 9:
     st.text("前期週間予定表")
 else:
     st.text("後期週間予定表")
+
+df = pd.DataFrame({
+    "時間割":["1-2","3-4","5-6","7-8"],
+    "月曜日":["","","キャリアデザイン3","英語"],
+    "火曜日":["クラウドコンピューティング","クラウドコンピューティング","Linux実習2","Linux実習2"],
+    "水曜日":["AIシステム開発","AIシステム開発","機械学習",""],
+    "木曜日":["","","サーバーサイドプログラム2","サーバーサイドプログラム2"],
+    "金曜日":["","ロジカルシンキング","サーバーサイドプログラム2","HR"]
+})
+st.dataframe(df,hide_index=True)
+
